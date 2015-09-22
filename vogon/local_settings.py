f988024c3eaf5f327611a7c157eace7d8d38ad80
Erick Peirson <erick.peirson@asu.edu>
2015-09-21 23:34:16 -0500

    changed local_settings database to sqlite

--- vogon/local_settings.py
+++ vogon/local_settings.py
@@ -86,15 +86,21 @@ WSGI_APPLICATION = 'vogon.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'vogon',
+#         'USER': 'vogon',
+#         'PASSWORD': 'vogon',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vogon',
-        'USER': 'vogon',
-        'PASSWORD': 'vogon',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
