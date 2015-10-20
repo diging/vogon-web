002e374cf9d895c2131ac8442d10d7de0505e22e
Shivam Prakash <prakash.shivam@gmail.com>
2015-10-20 15:03:11 -0700

    [VGNWB-43]Fixed Indentation

--- annotations/forms.py
+++ annotations/forms.py
@@ -24,29 +24,29 @@ class RegistrationForm(forms.Form):
 	"""
 	Gives user form of signup and validates it.
 	"""
-    username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
-    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password (again)"))
+	username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
+	email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
+	password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
+	password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password (again)"))
 
-    def clean_username(self):
+	def clean_username(self):
 		"""
 		Validates username.
 		"""
-        try:
-            user = User.objects.get(username__iexact=self.cleaned_data['username'])
-        except User.DoesNotExist:
-            return self.cleaned_data['username']
-        raise forms.ValidationError(_("The username already exists. Please try another one."))
+		try:
+			user = User.objects.get(username__iexact=self.cleaned_data['username'])
+		except User.DoesNotExist:
+			return self.cleaned_data['username']
+		raise forms.ValidationError(_("The username already exists. Please try another one."))
 
-    def clean(self):
+	def clean(self):
 		"""
 		Validates the values inserted in Password and Confirm Password field are same.
 		"""
-        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
-            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-                raise forms.ValidationError(_("The two password fields did not match."))
-        return self.cleaned_data
+		if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+			if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+				raise forms.ValidationError(_("The two password fields did not match."))
+		return self.cleaned_data
 
 def validatefiletype(file):
 	"""
