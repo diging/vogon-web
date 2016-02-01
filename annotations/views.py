06a8bf12c49b4e68c056cb06ce185925057a6938
Vivek Narsimha Dondeti <vdondeti@asu.edu>
2016-02-01 11:31:21 -0700

    Incluing the list user method in views

--- annotations/views.py
+++ annotations/views.py
@@ -274,7 +274,6 @@ def text(request, textid):
     })
     return HttpResponse(template.render(context))
 
-
 ### REST API class-based views.
 
 
@@ -282,6 +281,32 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = VogonUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    def list(self, request):
+        """
+        List all the users of Vogon web
+        """
+        template = loader.get_template('annotations/contributers.html')
+        userList = []
+        # text_list = Text.objects.all()
+        for user in queryset:
+            userList.append(user)
+        paginator = Paginator(userList, 25)
+
+        page = request.GET.get('page')
+        try:
+            users = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            users = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            users = paginator.page(paginator.num_pages)
+
+        context = {
+            'user_list': users,
+            'user': request.user,
+        }
+        return HttpResponse(template.render(context))
 
 
 class RepositoryViewSet(viewsets.ModelViewSet):
