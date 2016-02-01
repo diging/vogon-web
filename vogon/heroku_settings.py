4278bb902902a4f1c1dcf764a84b0924586694ed
Erick Peirson <erick.peirson@asu.edu>
2016-02-01 15:52:24 -0500

    restored heroku settings

--- vogon/heroku_settings.py
+++ vogon/heroku_settings.py
@@ -140,8 +140,8 @@ SUBPATH = '/'
 
 # Parse database configuration from $DATABASE_URL
 # DATABASES['default'] =  dj_database_url.config()
-# DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-# DATABASES['default']['NAME'] = 'vogon'
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+DATABASES['default']['NAME'] = 'vogon'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
