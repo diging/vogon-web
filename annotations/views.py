5d811aa52180dfd8cf44030b77188068b341b82f
= <vdondeti@asu.edu>
2016-02-05 17:05:54 -0700

    Paging done with queryset insted of list

--- annotations/views.py
+++ annotations/views.py
@@ -218,6 +218,49 @@ def list_texts(request):
     }
     return HttpResponse(template.render(context))
 
+@login_required
+def list_user(request):
+    """
+    List all the users of Vogon web
+    """
+
+    template = loader.get_template('annotations/contributors.html')
+
+    #Default Sort
+    sort = "user_name"
+    sort_order = "asc"
+    #To map the column sort parameter from UI to the column name in DB
+    sort_dict = {"user_name":"username", "name":"full_name",
+     "aff":"affiliation", "loc":"location"}
+
+    if(request.GET.get('sort') != None):
+        sort = request.GET.get('sort')
+    if(request.GET.get('sort_order') != None):
+        sort_order = request.GET.get('sort_order')
+
+    sort_column = sort_dict[sort]
+
+    if(sort_order == "dsc"):
+        queryset = VogonUser.objects.all().order_by("-"+sort_column)
+    paginator = Paginator(queryset, 5)
+
+    page = request.GET.get('page')
+    try:
+        users = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        users = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        users = paginator.page(paginator.num_pages)
+
+    context = {
+        'sort_order' : sort_order
+        'sort_column' : sort,
+        'user_list': users,
+        'user': request.user,
+    }
+    return HttpResponse(template.render(context))
 
 @login_required
 def collection_texts(request, collectionid):
@@ -281,39 +324,6 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = VogonUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
-    counter = 1
-    def list(self, request):
-        """
-        List all the users of Vogon web
-        """
-        #To map the column sort parameter from UI to the column name in DB
-        sortDict = {"user_name":"username", "name":"full_name",
-         "aff":"affiliation", "loc":"location"}
-
-        sort = request.GET.get('sort')
-        if(sort != None):
-            sort_by = sortDict[sort]
-            self.queryset = VogonUser.objects.all().order_by(sort_by)
-        user_list = list(self.queryset)
-        template = loader.get_template('annotations/contributors.html')
-        paginator = Paginator(user_list, 25)
-
-        page = request.GET.get('page')
-        try:
-            users = paginator.page(page)
-        except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
-            users = paginator.page(1)
-        except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver last page of results.
-            users = paginator.page(paginator.num_pages)
-
-        context = {
-            'user_list': users,
-            'user': request.user,
-        }
-        return HttpResponse(template.render(context))
-
 
 
 class RepositoryViewSet(viewsets.ModelViewSet):
