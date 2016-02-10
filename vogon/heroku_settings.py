3cd3129bd54ce64fd4cc70bd63f59109e25e9ba6
Erick Peirson <erick.peirson@asu.edu>
2016-02-10 18:07:37 -0500

    config

--- vogon/heroku_settings.py
+++ vogon/heroku_settings.py
@@ -93,12 +93,12 @@ WSGI_APPLICATION = 'vogon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vogon',
-        'USER': 'vogon',
-        'PASSWORD': 'vogon',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'vogon',
+        # 'USER': 'vogon',
+        # 'PASSWORD': 'vogon',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
