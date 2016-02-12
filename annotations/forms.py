da73827fa7122619125a7f9d95c0e4ad6797e004
Nitin Karda <kardanitin@gmail.com>
2016-02-12 09:37:41 -0700

    Making changes to form to include location, fullName, affliation

--- annotations/forms.py
+++ annotations/forms.py
@@ -31,6 +31,9 @@ class RegistrationForm(forms.Form):
 	email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
 	password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
 	password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password (again)"))
+	affiliation = forms.CharField(widget=forms.TextInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Location"))
+	location = forms.CharField(widget=forms.TextInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Affliation"))
+	full_name = forms.CharField(widget=forms.TextInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Full name"))
 
 	def clean_username(self):
 		"""
@@ -49,7 +52,6 @@ class RegistrationForm(forms.Form):
 		if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
 			if self.cleaned_data['password1'] != self.cleaned_data['password2']:
 				raise forms.ValidationError(_("The two password fields did not match."))
-		return self.cleaned_data
 
 def validatefiletype(file):
 	"""
