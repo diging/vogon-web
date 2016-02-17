d848ea9c8aaa0a13dc084e4f81d9700c7b184f5c
Erick Peirson <erick.peirson@asu.edu>
2016-02-17 14:26:28 -0500

    postdeploy

--- postdeploy.sh
+++ postdeploy.sh
@@ -3,4 +3,5 @@ export CELERY_TASK_SERIALIZER=json
 export DJANGO_SETTINGS_MODULE='vogon.heroku_settings'
 
 python manage.py migrate
+python manage.py loaddata vogonuser.xml
 python manage.py loaddata vogonweb-data.xml
