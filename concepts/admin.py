752c06d408c9f4f871485e054a72f7741168d5ae
YoganandaKishore <diging_yogi@sols-jessima-06.mobile.asu.edu>
2016-03-09 11:26:51 -0700

    Revert "Issue fix"
    This reverts commit bed85d2fa926d3191a792a14a8aa7532701fe5f6.

--- concepts/admin.py
+++ concepts/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-from django.contrib.contenttypes.models import ContentType
-from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
 from .models import *
 
 from . import authorities
@@ -11,31 +8,17 @@ def resolve(modeladmin, request, queryset):
         authorities.resolve(type(obj), obj)
 resolve.verbose_name = 'resolve selected concepts'
 
-def merge_concepts(self, request, queryset):
-    return render_to_response('admin/merge_concepts.html', {'concepts': queryset})
-    add_tag.short_description = "Merge Concepts"
-
 class ConceptAdmin(admin.ModelAdmin):
     model = Concept
-    search_fields = (   'label',    )
-    list_display = (    'label', 'resolved','concept_state'  )
-    actions = ( resolve,merge_concepts,    )
-    list_filter=('concept_state',)
+    search_fields = ('label',)
+    list_display = ('label', 'resolved', 'concept_state', 'typed',)
+    actions = (resolve,)
+    list_filter=('concept_state', 'typed',)
 
 class TypeAdmin(admin.ModelAdmin):
     model = Type
-    list_display = (    'label', 'resolved'  )
 
-# def export_selected_objects(modeladmin, request, queryset):
-#     selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
-#     ct = ContentType.objects.get_for_model(queryset.model)
-#     return HttpResponseRedirect("/export/?ct=%s&ids=%s" % (ct.pk, ",".join(selected)))
-#
-# class MergeAdmin(admin.ModelAdmin):
-#     model = Merge
-#     actions = ( export_selected_objects,    )
-#     list_display = (    'label', 'resolved'  )
+    list_display = ('label', 'resolved',)
 
 admin.site.register(Concept, ConceptAdmin)
 admin.site.register(Type, TypeAdmin)
-# admin.site.register(Merge, MergeAdmin)
