a31fb0f8a11869797f12dc25fc8a4c9637548f1a
YoganandaKishore <diging_yogi@sols-jessima-06.mobile.asu.edu>
2016-03-15 11:54:34 -0700

    Making the changes according to code review comments

--- concepts/admin.py
+++ concepts/admin.py
@@ -3,8 +3,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from .models import *
-
 from . import authorities
+
 
 def resolve(modeladmin, request, queryset):
     for obj in queryset:
@@ -12,35 +12,36 @@ def resolve(modeladmin, request, queryset):
 resolve.verbose_name = 'resolve selected concepts'
 
 def merge_concepts(modeladmin, request, queryset):
+    """
+    This method gets called when we click on "Merge Concepts" action after we select the concepts
+    from the list of concepts
+    :param modeladmin:
+    :param request:
+    :param queryset:
+    :return:
+    """
 
     opts = modeladmin.model._meta
     app_label = opts.app_label
-    RESOLVED_STATUS = 'Resolved'
-    resolvedCount = 0
-    resolvedConcept = None
-
-    for item in queryset:
-        if item.concept_state == RESOLVED_STATUS:
-            resolvedCount += 1
-            resolvedConcept = item
-
-    context = {
-        "resolvedConcept":resolvedConcept,
-        "opts": opts,
-        "app_label": app_label
-    }
+    RESOLVED_STATUS = Concept.RESOLVED
+    resolvedConceptCount = queryset.filter(concept_state = RESOLVED_STATUS).count()
 
     #VGNWB-121 gets called only when there is only one resolved concept
-    if resolvedCount == 1:
+    if resolvedConceptCount == 1:
+        resolvedConceptsList = queryset.filter(concept_state = RESOLVED_STATUS).values()
+        #As there will be only one element in the list
+        resolvedConcept = resolvedConceptsList[0]
+        resolvedConceptName = resolvedConcept['label']
+
+        context = {
+        "resolvedConcept": resolvedConceptName,
+        "opts": opts,
+        "app_label": app_label
+        }
         return render_to_response('admin/merge_concepts_resolved.html', context)
 
     return render_to_response('admin/merge_concepts.html', {'concepts': queryset})
-merge_concepts.verbose_name = "Merge Concepts"
 
-
-#def merge_concepts(self, request, queryset):
-#    return render_to_response('admin/merge_concepts.html', {'concepts': queryset})
-#merge_concepts.verbose_name = "Merge Concepts"
 
 class ConceptAdmin(admin.ModelAdmin):
     model = Concept
