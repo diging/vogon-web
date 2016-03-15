968c035d3e8444e385b4117408f246250f95947b
YoganandaKishore <diging_yogi@sols-jessima-06.mobile.asu.edu>
2016-03-15 09:12:43 -0700

    Making the required changes for VGNWB-121

--- concepts/admin.py
+++ concepts/admin.py
@@ -11,9 +11,36 @@ def resolve(modeladmin, request, queryset):
         authorities.resolve(type(obj), obj)
 resolve.verbose_name = 'resolve selected concepts'
 
-def merge_concepts(self, request, queryset):
+def merge_concepts(modeladmin, request, queryset):
+
+    opts = modeladmin.model._meta
+    app_label = opts.app_label
+    RESOLVED_STATUS = 'Resolved'
+    resolvedCount = 0
+    resolvedConcept = None
+
+    for item in queryset:
+        if item.concept_state == RESOLVED_STATUS:
+            resolvedCount += 1
+            resolvedConcept = item
+
+    context = {
+        "resolvedConcept":resolvedConcept,
+        "opts": opts,
+        "app_label": app_label
+    }
+
+    #VGNWB-121 gets called only when there is only one resolved concept
+    if resolvedCount == 1:
+        return render_to_response('admin/merge_concepts_resolved.html', context)
+
     return render_to_response('admin/merge_concepts.html', {'concepts': queryset})
 merge_concepts.verbose_name = "Merge Concepts"
+
+
+#def merge_concepts(self, request, queryset):
+#    return render_to_response('admin/merge_concepts.html', {'concepts': queryset})
+#merge_concepts.verbose_name = "Merge Concepts"
 
 class ConceptAdmin(admin.ModelAdmin):
     model = Concept
