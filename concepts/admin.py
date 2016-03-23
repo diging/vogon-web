51da5a8e577bc07998f108cca5cb81c259e13e6d
YoganandaKishore <diging_yogi@sols-jessima-06.local>
2016-03-23 11:51:36 -0700

    making required changes to integrate action behaviour

--- concepts/admin.py
+++ concepts/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import helpers
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.template.context_processors import csrf
+from django.template import RequestContext
 from .models import *
 from . import authorities
 
@@ -20,30 +23,49 @@ def merge_concepts(modeladmin, request, queryset):
     :param queryset:
     :return:
     """
-    
-    opts = modeladmin.model._meta
-    app_label = opts.app_label
+    #return render_to_response('admin/Sample.html')
     RESOLVED_STATUS = Concept.RESOLVED
-    resolvedConceptCount = queryset.filter(concept_state = RESOLVED_STATUS).count()
+
+
+    if 'post' in request.POST:
+            # process the queryset here
+
+        unResolvedConceptsList = queryset.exclude(concept_state = RESOLVED_STATUS).values()
+        resolvedConceptsList = queryset.filter(concept_state = RESOLVED_STATUS).values()
+        resolvedConcept = resolvedConceptsList[0]
+
+        for i in range(0,len(unResolvedConceptsList)):
+            presentConcept = unResolvedConceptsList.get(i)
+            presentConcept.merged_with = resolvedConcept
+            presentConcept.concept_state = Concept.REJECTED
+
+        return render_to_response('admin/Sample.html')
+    else:
+        opts = modeladmin.model._meta
+        app_label = opts.app_label
+
+        resolvedConceptCount = queryset.filter(concept_state = RESOLVED_STATUS).count()
 
     #VGNWB-121 gets called only when there is only one resolved concept
-    if resolvedConceptCount == 1:
-        resolvedConceptsList = queryset.filter(concept_state = RESOLVED_STATUS).values()
-        unResolvedConceptsList = queryset.exclude(concept_state = RESOLVED_STATUS).values()
+        if resolvedConceptCount == 1:
+            resolvedConceptsList = queryset.filter(concept_state = RESOLVED_STATUS).values()
+            unResolvedConceptsList = queryset.exclude(concept_state = RESOLVED_STATUS).values()
 
-        #As there will be only one element in the list
-        resolvedConcept = resolvedConceptsList[0]
-        resolvedConceptName = resolvedConcept['label']
+            #As there will be only one element in the list
+            resolvedConcept = resolvedConceptsList[0]
+            resolvedConceptName = resolvedConcept['label']
 
-        context = {
-        "resolvedConcept": resolvedConceptName,
-        "opts": opts,
-        "app_label": app_label,
-        "unResolvedConcepts": unResolvedConceptsList
-        }
-        return render_to_response('admin/merge_concepts_resolved.html', context)
+            context = {
+                "resolvedConcept": resolvedConceptName,
+                "opts": opts,
+                "app_label": app_label,
+                "unResolvedConcepts": unResolvedConceptsList ,
+                "path" : request.get_full_path(),
 
-    return render_to_response('admin/merge_concepts.html', {'concepts': queryset})
+            }
+            return render_to_response('admin/merge_concepts_resolved.html', context, context_instance = RequestContext(request),)
+
+        return render_to_response('admin/merge_concepts.html', {'concepts': queryset})
 
 
 class ConceptAdmin(admin.ModelAdmin):
