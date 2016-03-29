5d627df1f97a09716111ca825df7f90fc3cfaf88
Erick Peirson <erick.peirson@asu.edu>
2016-03-29 16:47:57 -0400

    hotfix: VGNWB-156 and VGNWB-155 data migration errors

--- vogon/local_settings.py
+++ vogon/local_settings.py
@@ -92,22 +92,22 @@ WSGI_APPLICATION = 'vogon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vogonweb',
-        'USER': 'vogonweb',
-        'PASSWORD': 'vogonweb',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'vogonweb',
+#         'USER': 'vogonweb',
+#         'PASSWORD': 'vogonweb',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend', # default
