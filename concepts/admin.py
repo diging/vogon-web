54664c2dd6dfca90705876b357a8083e5edd58dd
YoganandaKishore <diging_yogi@sols-jessima-06.mobile.asu.edu>
2016-03-30 12:06:19 -0700

    Making changes to POST andsimplifying processing of concepts using queryset methods

--- concepts/admin.py
+++ concepts/admin.py
@@ -7,6 +7,7 @@ from django.template.context_processors import csrf
 from django.template import RequestContext
 from .models import *
 from . import authorities
+import django.forms as forms
 
 
 def resolve(modeladmin, request, queryset):
@@ -14,57 +15,53 @@ def resolve(modeladmin, request, queryset):
         authorities.resolve(type(obj), obj)
 resolve.verbose_name = 'resolve selected concepts'
 
+class SeriesForm(forms.Form):
+        _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
+
 def merge_concepts(modeladmin, request, queryset):
-    """
-    This method gets called when we click on "Merge Concepts" action after we select the concepts
-    from the list of concepts
-    :param modeladmin:
-    :param request:
-    :param queryset:
-    :return:
-    """
-    unResolvedConceptsList = queryset.exclude(concept_state = Concept.RESOLVED).values()
-    resolvedConceptsList = queryset.filter(concept_state = Concept.RESOLVED).values()
 
-    if 'POST' in request.POST:
-        # process the queryset here
+        unResolvedConceptsList = queryset.exclude(concept_state = Concept.RESOLVED)
+        resolvedConceptsList = queryset.filter(concept_state = Concept.RESOLVED)
+        resolvedConcept = resolvedConceptsList[0]
 
-        unResolvedConceptsCount = unResolvedConceptsList.count()
+        if request.method == 'POST':
 
-        for i in range(0,unResolvedConceptsCount):
-             presentConcept = unResolvedConceptsList.get(i)
-             presentConcept.merged_with = unResolvedConceptsList[0]['label']
-             presentConcept.concept_state = Concept.REJECTED
+            # process the queryset here
+            unResolvedConceptsList.update(concept_state=Concept.REJECTED)
+            unResolvedConceptsList.update(merged_with=resolvedConcept)
 
-    else:
-        opts = modeladmin.model._meta
-        app_label = opts.app_label
-        resolvedConceptCount = queryset.filter(concept_state = Concept.RESOLVED).count()
+        else:
+            opts = modeladmin.model._meta
+            app_label = opts.app_label
+            resolvedConceptCount = queryset.filter(concept_state = Concept.RESOLVED).count()
 
-    #VGNWB-121 gets called only when there is only one resolved concept
-        if resolvedConceptCount == 1:
+            #VGNWB-121 gets called only when there is only one resolved concept
+            if resolvedConceptCount == 1:
 
             #As there will be only one element in the list
+                form = SeriesForm(initial={'_selected_action': request.POST.getlist(admin.ACTION_CHECKBOX_NAME)})
 
-            context = {
-                "resolvedConcept": resolvedConceptsList[0]['label'],
-                "opts": opts,
-                "app_label": app_label,
-                "unResolvedConcepts": unResolvedConceptsList ,
-                "path" : request.get_full_path(),
+                context = {
+                    "resolvedConcept": resolvedConcept,
+                    "opts": opts,
+                    "app_label": app_label,
+                    "unResolvedConcepts": unResolvedConceptsList ,
+                    "path" : request.get_full_path(),
+                    "form" :form ,
+                }
 
-            }
-            return render_to_response('admin/merge_concepts_resolved.html', context, context_instance = RequestContext(request),)
+                return render_to_response('admin/merge_concepts_resolved.html', context, context_instance = RequestContext(request))
 
-        return render_to_response('admin/merge_concepts.html', {'concepts': queryset})
+            return render_to_response('admin/merge_concepts.html', {'concepts': queryset})
 
 
 class ConceptAdmin(admin.ModelAdmin):
     model = Concept
     search_fields = ('label',)
     list_display = ('label', 'resolved', 'concept_state', 'typed',)
-    actions = (resolve, merge_concepts,)
+    actions = (resolve,merge_concepts)
     list_filter=('concept_state', 'typed',)
+    #opts = modeladmin.model._meta
 
 class TypeAdmin(admin.ModelAdmin):
     model = Type
