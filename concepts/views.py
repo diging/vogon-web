af3e74c97107cb3ccafb54740a811c2ab80437bc
Nitin Karda <kardanitin@gmail.com>
2016-04-18 00:33:32 -0700

    added types view

--- concepts/views.py
+++ concepts/views.py
@@ -9,27 +9,14 @@ def list_concept_types(request):
     """
     List all of the concept types
     """
-    template = loader.get_template('concepts/list_types.html')
+    types = Concept.objects.all().values('typed__label','typed').distinct()
 
-    types = Concept.object.all().values('typed__label').distinct()
-
-    paginator = Paginator(types, 15)
-
-    page = request.GET.get('page')
-    try:
-        texts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        texts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        texts = paginator.page(paginator.num_pages)
 
     context = {
         'types': types,
         'user': request.user,
     }
-    return HttpResponse(template.render(context))
+    return HttpResponse(str(context))#template.render(context))
 
 
 def type(request, type_id):
