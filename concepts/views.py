1e7e536d8e50bc0845668b121f32cb27f3b04f7d
Nitin Karda <kardanitin@gmail.com>
2016-04-18 00:26:11 -0700

    added types view

--- concepts/views.py
+++ concepts/views.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 from django.template import loader
 from guardian.shortcuts import get_objects_for_user
+from concepts.models import Concept
 
 
 def list_concept_types(request):
@@ -10,12 +11,9 @@ def list_concept_types(request):
     """
     template = loader.get_template('concepts/list_types.html')
 
-    text_list = get_objects_for_user(request.user, 'annotations.view_text')
+    types = Concept.object.all().values('typed__label').distinct()
 
-    order_by = request.GET.get('order_by', 'title')
-    text_list = text_list.order_by(order_by)
-
-    paginator = Paginator(text_list, 15)
+    paginator = Paginator(types, 15)
 
     page = request.GET.get('page')
     try:
@@ -28,8 +26,7 @@ def list_concept_types(request):
         texts = paginator.page(paginator.num_pages)
 
     context = {
-        'types': texts,
-        'order_by': order_by,
+        'types': types,
         'user': request.user,
     }
     return HttpResponse(template.render(context))
@@ -37,32 +34,3 @@ def list_concept_types(request):
 
 def type(request, type_id):
     pass
-
-
-# Create your views here.
-# def home(request):
-#     """
-#
-#     Provides a landing page containing information about the application
-#     for user who are not authenticated
-#
-#     LoggedIn users are redirected to the dashboard view
-#     ----------
-#     request : HTTPRequest
-#         The request for application landing page.
-#     Returns
-#     ----------
-#     :template:
-#         Renders landing page for non-loggedin user and
-#         dashboard view for loggedin users.
-#     """
-#     template = loader.get_template('annotations/home.html')
-#     user_count = VogonUser.objects.filter(is_active=True).count()
-#     text_count = Text.objects.all().count()
-#     relation_count = Relation.objects.count()
-#     context = RequestContext(request, {
-#         'user_count': user_count,
-#         'text_count': text_count,
-#         'relation_count': relation_count
-#     })
-#     return HttpResponse(template.render(context))
