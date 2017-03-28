2a8f3b21f154d18e88b5c62db55c2bd8c8507a52
Erick Peirson <erick.peirson@asu.edu>
2017-03-28 15:58:53 -0400

    travis

--- vogon/settings.py
+++ vogon/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'secretsecret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(os.environ.get('DEBUG', 'False'))
