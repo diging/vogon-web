afc5e4cb0d52984af83fbe8d06faaa7c1191e88c
Erick Peirson <erick.peirson@asu.edu>
2017-04-05 15:56:28 -0400

    hotfix for rest paths

--- annotations/static/annotations/js/annotator/annotator.js
+++ annotations/static/annotations/js/annotator/annotator.js
@@ -107,16 +107,16 @@ var clearMouseTextSelection = function () {
   *****************************************************************************/
 Vue.http.headers.common['X-CSRFTOKEN'] = Cookie.get('csrftoken');
 
-var Appellation = Vue.resource('/rest/appellation{/id}');
-var DateAppellation = Vue.resource('/rest/dateappellation{/id}');
-var Relation = Vue.resource('/rest/relationset{/id}');
-var Concept = Vue.resource('/rest/concept{/id}', {}, {
-    search: {method: 'GET', url: '/rest/concept/search'}
+var Appellation = Vue.resource(BASE_URL + '/rest/appellation{/id}');
+var DateAppellation = Vue.resource(BASE_URL + '/rest/dateappellation{/id}');
+var Relation = Vue.resource(BASE_URL + '/rest/relationset{/id}');
+var Concept = Vue.resource(BASE_URL + '/rest/concept{/id}', {}, {
+    search: {method: 'GET', url: BASE_URL + '/rest/concept/search'}
 });
-var RelationTemplateResource = Vue.resource('/relationtemplate{/id}/', {}, {
-    create: {method: 'POST', url: '/relationtemplate{/id}/create/'}
+var RelationTemplateResource = Vue.resource(BASE_URL + '/relationtemplate{/id}/', {}, {
+    create: {method: 'POST', url: BASE_URL + '/relationtemplate{/id}/create/'}
 });
-var ConceptType = Vue.resource('/rest/type{/id}');
+var ConceptType = Vue.resource(BASE_URL + '/rest/type{/id}');
 
 /******************************************************************************
   *         Components!
