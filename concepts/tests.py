773563d8d171ab38b7cec2ff21dc71f8445e121c
Erick Peirson <erick.peirson@asu.edu>
2017-05-01 15:49:36 -0400

    removed outdated unittest

--- concepts/tests.py
+++ concepts/tests.py
@@ -26,40 +26,6 @@ def reconnect_signal(signal, receiver, sender):
     connect(receiver, sender=sender)
 
 
-class TestConceptSearch(TestCase):
-    def test_search(self):
-        concepts = search('Bradshaw', pos='noun')
-        self.assertIsInstance(concepts, list)
-        self.assertIsInstance(concepts[0], Concept)
-
-
-
-
-
-# class TestConceptResolve(TestCase):
-#     def setUp(self):
-#         disconnect_signal(post_save, concept_post_save_receiver, Concept)
-#         disconnect_signal(post_save, type_post_save_receiver, Type)
-#
-#     def test_resolve(self):
-#         c = Concept(uri="http://www.digitalhps.org/concepts/CONe5b55803-1ef6-4abe-b81c-1493e97421df",
-#                     authority="ConceptpowerAuthority")
-#         c.save()
-#
-#         resolve(Concept, c)
-#         resolve(Type, c.typed)
-#
-#         self.assertTrue(hasattr(c, 'typed'))
-#         self.assertFalse(c.typed is None)
-#         self.assertTrue(c.resolved)
-#         self.assertTrue(c.typed.resolved)
-#         self.assertEqual(c.pos, 'noun')
-#
-#     def tearDown(self):
-#         reconnect_signal(post_save, concept_post_save_receiver, Concept)
-#         reconnect_signal(post_save, type_post_save_receiver, Type)
-
-
 class TestConceptLifeCycle(TestCase):
     """
     The :class:`.ConceptLifecycle` guides :class:`.Concept`\s through their
