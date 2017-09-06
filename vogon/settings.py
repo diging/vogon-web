f27087e26e8c84624d9fe49ac4c0260218de40f9
tjquinn1 <tquinn2035@gmail.com>
2017-09-06 04:17:05 -0700

    Story/vgnwb 219 (#120)
    * [VGNWB-219] moved type id to settings. Allowed them to be configurable through export statements and added default values.
    
    * [VGNWB-219] updated ready
    
    * [VGNWB-219] removed import settings
    
    * [VGNWB-219] removed import and changed lifecycle to concept
    
    * [VGNWB-219] updated concept type
    
    * [VGNWB-219] Made variable names more verbose
    
    * [VGNWB-219] made the enviroment names more verbose and removed inputrc file
    
    * [VGNWB-219] removed the .inputrc that I thought I had already removed

--- vogon/settings.py
+++ vogon/settings.py
@@ -245,3 +245,15 @@ LOGIN_URL = BASE_URL + 'login/github/'
 LOGLEVEL = os.environ.get('LOGLEVEL', 'DEBUG')
 
 SESSION_COOKIE_NAME = 'vogon'
+
+
+# Concept types
+PERSONAL_CONCEPT_TYPE = os.environ.get('PERSONAL_CONCEPT_TYPE', '986a7cc9-c0c1-4720-b344-853f08c136ab')
+CORPORATE_CONCEPT_TYPE = os.environ.get('CORPORATE_CONCEPT_TYPE', '3fc436d0-26e7-472c-94de-0b712b66b3f3')
+GEOGRAPHIC_CONCEPT_TYPE = os.environ.get('GEOGRAPHIC_CONCEPT_TYPE', 'dfc95f97-f128-42ae-b54c-ee40333eae8c')
+
+CONCEPT_TYPES = {
+    'viaf:personal': PERSONAL_CONCEPT_TYPE,    # E21 Person
+    'viaf:corporate': CORPORATE_CONCEPT_TYPE,   # E40 Legal Body
+    'viaf:geographic': GEOGRAPHIC_CONCEPT_TYPE,    # E53 Place
+}
