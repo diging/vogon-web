41baffc71f292bcb4c7fa471b84c5c17e95e8ddc
Tjquinn1 <tquinn2035@gmail.com>
2017-10-16 13:56:04 -0700

    [VGNWB-223]Fixed concept filter from resetting.

--- concepts/views.py
+++ concepts/views.py
@@ -12,6 +12,7 @@ from concepts.authorities import ConceptpowerAuthority, update_instance
 from django.contrib.auth.decorators import login_required
 import re, urllib, string
 from unidecode import unidecode
+from urllib import urlencode
 
 
 
@@ -103,6 +104,13 @@ def concepts(request):
 
     paginator = Paginator(qs, 20)
     page = request.GET.get('page')
+
+    data = filtered.form.cleaned_data
+    params_data = {}
+    for key, value in data.items():
+        if value is not None:
+            params_data[key] = value
+
     try:
         concepts = paginator.page(page)
     except PageNotAnInteger:
@@ -117,6 +125,7 @@ def concepts(request):
         'concepts': concepts,
         'filter': filtered,
         'path': urllib.quote_plus(request.path + '?' + request.GET.urlencode()),
+        'params_data': urlencode(params_data),
     }
 
     return render(request, 'annotations/concepts.html', context)
