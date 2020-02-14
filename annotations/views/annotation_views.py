de6668ec33036dc05957a8234fd5d19010512913
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2020-02-14 11:21:30 -0700

    [VGNWB-312] Remove annotation views and templates

--- annotations/views/annotation_views.py
+++ annotations/views/annotation_views.py
@@ -1,22 +1,12 @@
-import json
 import itertools as it
-from urllib.parse import urlencode
-from requests import Response
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import get_object_or_404, render
-from django.views.decorators.csrf import ensure_csrf_cookie
-from django.core import serializers
-from django_filters import FilterSet
-from django_filters.rest_framework import DjangoFilterBackend
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.response import Response as RestResponse
-from rest_framework.decorators import api_view, action
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from annotations.models import Relation, Appellation, VogonUser, Text, RelationSet, TextCollection, Repository, Appellation
 from annotations.annotators import annotator_factory
-from annotations.serializers import (RelationSerializer, RelationSetSerializer,
+from annotations.serializers import (RelationSetSerializer,
     ProjectSerializer, UserSerializer, Text2Serializer)
 from annotations.filters import RelationSetFilter
 from annotations.tasks import submit_relationsets_to_quadriga
@@ -37,7 +27,7 @@ class RelationSetViewSet(viewsets.ModelViewSet):
         else:
             relations = serializer(queryset, many=True).data
             
-        return RestResponse(relations)
+        return Response(relations)
 
     def get_paginated_response(self, data, meta):
         extra = {}
@@ -48,7 +38,7 @@ class RelationSetViewSet(viewsets.ModelViewSet):
                 'projects': ProjectSerializer(projects, many=True).data,
                 'users': UserSerializer(users, many=True).data
             }
-        return RestResponse({
+        return Response({
             'count':len(self.get_queryset()),
             'results': data,
             **extra
@@ -86,116 +76,31 @@ class RelationSetViewSet(viewsets.ModelViewSet):
                 submit_relationsets_to_quadriga(rsets, text.id, request.user.id, **kwargs)
 
 
-        return RestResponse({})
+        return Response({})
 
-#@login_required
-#@ensure_csrf_cookie
-@api_view(['GET', 'POST'])
-def annotate(request, text_id):
-    text = get_object_or_404(Text, pk=text_id)
-    annotator = annotator_factory(request, text)
-    data = annotator.render()
-    appellations = Appellation.objects.filter(occursIn=text.id)
-    content = data['content'].decode("utf-8")
-    data['content'] = content
-    project = TextCollection.objects.get(id=data['project'])
-    data['project'] = project
-    data['appellations'] = appellations
-    print(appellations)
-    data['relations'] = Relation.objects.filter(occursIn=text.id)
-    relationsets = RelationSet.objects.filter(
-        occursIn=text.id, 
-        createdBy=request.user, 
-        submitted=False,
-    )
-    relationsets = [x for x in relationsets if x.ready()]
-    data['pending_relationsets'] = relationsets
-    serializer = Text2Serializer(data, context={'request': request})
-    return RestResponse(serializer.data)
+class AnnotationViewSet(viewsets.ViewSet):
+    queryset = Text.objects.all()
 
-
-@login_required
-def annotation_display(request, text_id):
-    text = get_object_or_404(Text, pk=text_id)
-    annotator = annotator_factory(request, text)
-    return annotator.render_display()
-
-@login_required
-def annotate_image(request, text_id):
-    template = "annotations/annotate_image.html"
-    text = Text.objects.get(pk=text_id)
-
-    return render(request, template, context)
-
-
-def relations(request):
-    from annotations.filters import RelationSetFilter
-
-
-    filtered = RelationSetFilter(request.GET, queryset=RelationSet.objects.all())
-    qs = filtered.qs
-
-    paginator = Paginator(qs, 40)
-    page = request.GET.get('page')
-
-    data = filtered.form.cleaned_data
-    params_data = {}
-    for key, value in list(data.items()):
-        if key in ('createdBy', 'project'):
-            if value is not None and hasattr(value, 'id'):
-                params_data[key] = value.id
-        elif key in ('createdAfter', 'createdBefore'):
-            if value is not None:
-                value = '{0.month}/{0.day}/{0.year}'.format(value)
-                params_data[key] = value
-        else:
-            params_data[key] = value
-
-
-    try:
-        relations = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        relations = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        relations = paginator.page(paginator.num_pages)
-    count = paginator.count
-    previous = None if not relations.has_previous() else relations.previous_page_number()
-    next =None if not relations.has_next() else relations.next_page_number()
-    relationsserializer = RelationSerializer(relations, many=True)
-    context = {
-        'paginator': {
-            'count':count,
-            'previous':previous,
-            'next':next
-        },
-        'relations': relationsserializer.data,
-        'params': request.GET.urlencode(),
-        'filter': filtered.data,
-        'params_data': urlencode(params_data),
-        }
-    return Response(json.dumps(context), content_type='application/json')
-
-
-def relations_graph(request):
-    from annotations.filters import RelationSetFilter
-    from annotations.views.network_views import generate_network_data_fast
-    qs = RelationSet.objects.all()
-    filtered = RelationSetFilter(request.GET, queryset=qs)
-    qs = filtered.qs
-
-    if request.GET.get('mode', None) == 'data':
-
-        nodes, edges = generate_network_data_fast(qs)
-        return JsonResponse({'elements': list(nodes.values()) + list(edges.values())})
-    # relationsserializer = RelationSerializer(relations, many=True)
-    relationsvalue= relations(request)
-    context = {
-        'relations': relationsvalue.json(),
-        'filter': filtered.data,
-        'data_path': request.path + '?' + request.GET.urlencode() + '&mode=data',
-        'params': request.GET.urlencode(),
-    }
-
-    return HttpResponse(json.dumps(context), content_type='application/json')
+    def retrieve(self, request, pk=None):
+        """
+        View to get all data related to annotate text
+        """
+        text = get_object_or_404(Text, pk=pk)
+        annotator = annotator_factory(request, text)
+        data = annotator.render()
+        appellations = Appellation.objects.filter(occursIn=text.id)
+        content = data['content'].decode("utf-8")
+        data['content'] = content
+        project = TextCollection.objects.get(id=data['project'])
+        data['project'] = project
+        data['appellations'] = appellations
+        data['relations'] = Relation.objects.filter(occursIn=text.id)
+        relationsets = RelationSet.objects.filter(
+            occursIn=text.id,
+            createdBy=request.user,
+            submitted=False,
+        )
+        relationsets = [x for x in relationsets if x.ready()]
+        data['pending_relationsets'] = relationsets
+        serializer = Text2Serializer(data, context={'request': request})
+        return Response(serializer.data)
--- annotations/views/project_views.py
+++ annotations/views/project_views.py
@@ -1,25 +1,16 @@
 """
 Provides project (:class:`.TextCollection`) -related views.
 """
-
-from django.shortcuts import get_object_or_404, render
-from django.contrib.auth.decorators import login_required
-from django.urls import reverse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.core.exceptions import PermissionDenied
-from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth import login, authenticate
-from django.conf import settings
+from django.shortcuts import get_object_or_404
 from django.db.models import Q, Count
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, serializers, status
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from repository.models import Repository
 from annotations.models import TextCollection, RelationSet, Text
-from annotations.forms import ProjectForm
 from annotations.serializers import TextCollectionSerializer, ProjectTextSerializer, ProjectSerializer
+from repository.models import Repository
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -82,167 +73,3 @@ class ProjectViewSet(viewsets.ModelViewSet):
             'count':len(self.filter_queryset(self.get_queryset())),
             'results': data
         })
-
-def view_project(request, project_id):
-    """
-    Shows details about a specific project owned by the current user.
-
-    Parameters
-    ----------
-    request : `django.http.requests.HttpRequest`
-    project_id : int
-
-    Returns
-    ----------
-    :class:`django.http.response.HttpResponse`
-    """
-
-    project = get_object_or_404(TextCollection, pk=project_id)
-    template = "annotations/project_details.html"
-
-    order_by = request.GET.get('order_by', 'title')
-    texts = project.texts.all().order_by(order_by)\
-                         .values('id', 'title', 'added', 'repository_id', 'repository_source_id')
-
-    paginator = Paginator(texts, 15)
-    page = request.GET.get('page')
-    try:
-        texts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        texts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        texts = paginator.page(paginator.num_pages)
-
-    from annotations.filters import RelationSetFilter
-
-    # for now let's remove this; it takes long to load the pages and there is a bug somewhere
-    # that throws an error sometimes [VGNWB-215]
-    #filtered = RelationSetFilter({'project': project.id}, queryset=RelationSet.objects.all())
-    #relations = filtered.qs
-
-    context = {
-        'user': request.user,
-        'title': project.name,
-        'project': project,
-        'texts': texts,
-        #'relations': relations,
-    }
-
-    return render(request, template, context)
-
-
-@login_required
-def edit_project(request, project_id):
-    """
-    Allow the owner of a project to edit it.
-
-    Parameters
-    ----------
-    project_id : int
-    request : `django.http.requests.HttpRequest`
-
-    Returns
-    ----------
-    :class:`django.http.response.HttpResponse`
-    """
-    template = "annotations/project_change.html"
-    project = get_object_or_404(TextCollection, pk=project_id)
-    if project.ownedBy.id != request.user.id:
-        raise PermissionDenied("Whoops, you're not supposed to be here!")
-
-    if request.method == 'POST':
-        form = ProjectForm(request.POST, instance=project)
-        if form.is_valid():
-            form.save()
-            redirect_target = reverse('view_project', args=(project.id,))
-            return HttpResponseRedirect(redirect_target)
-        else:
-            print((form.errors))
-    else:
-        form = ProjectForm(instance=project)
-
-    context = {
-        'user': request.user,
-        'title': 'Editing project: %s' % project.name,
-        'project': project,
-        'form': form,
-        'page_title': 'Edit project'
-    }
-    return render(request, template, context)
-
-
-@login_required
-def create_project(request):
-    """
-    Create a new project owned by the current (logged-in) user.
-
-    Parameters
-    ----------
-    request : `django.http.requests.HttpRequest`
-
-    Returns
-    ----------
-    :class:`django.http.response.HttpResponse`
-    """
-    template = "annotations/project_change.html"
-
-    if request.method == 'POST':
-        form = ProjectForm(request.POST)
-        if form.is_valid():
-            project = form.save(commit=False)
-            project.ownedBy = request.user
-            project.save()
-            redirect_target = reverse('view_project', args=(project.id,))
-            return HttpResponseRedirect(redirect_target)
-        else:
-            print((form.errors))
-    else:
-        form = ProjectForm()
-
-    context = {
-        'user': request.user,
-        'title': 'Create a new project',
-        'form': form,
-        'page_title': 'Create a new project'
-    }
-    return render(request, template, context)
-
-
-def list_projects(request):
-    """
-    All known projects.
-
-    Parameters
-    ----------
-    project_id : int
-    request : `django.http.requests.HttpRequest`
-
-    Returns
-    ----------
-    :class:`django.http.response.HttpResponse`
-    """
-
-    fields = [
-        'id',
-        'name',
-        'created',
-        'ownedBy__id',
-        'ownedBy__username',
-        'description',
-        'num_texts',
-        'num_relations',
-    ]
-    qs = TextCollection.objects.all()
-    qs = qs.annotate(num_texts=Count('texts'),
-                     num_relations=Count('texts__relationsets'))
-    qs = qs.values(*fields)
-
-    template = "annotations/project_list.html"
-    context = {
-        'user': request.user,
-        'title': 'Projects',
-        'projects': qs,
-    }
-    return render(request, template, context)
