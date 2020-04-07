8c4d81b0140963074551ea112abd3815c4438e01
tjquinn1 <tjquinn1@asu.edu>
2020-04-07 14:08:40 -0700

    adjust logging x2

--- vogon/settings.py
+++ vogon/settings.py
@@ -286,12 +286,17 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': './debug.log',
         },
     },
-    'root': {
-        'handlers': ['console'],
-        'level': 'WARNING',
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
     },
 }
