f2f639bbac97730ae94fd0dd8fd44086ba65f283
tjquinn1 <tjquinn1@asu.edu>
2020-04-15 16:03:48 -0700

    updated startup script

--- docker_scripts/vogon-backend-startup.sh
+++ docker_scripts/vogon-backend-startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 service redis-server start
 service supervisor start
+docker cp /diging2/docker/python-apps-docker/vogonPython3/vogon_docker_test/vogon-backend/.env_secrets vogon-compose_vogon_1:/.env_secrets
 #python manage.py runserver 0.0.0.0:8000
