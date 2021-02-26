f154595cec03a3744ea4ff2e00026b150c18e040
Taylor <tjquinn1@asu.edu>
2021-02-26 11:52:38 -0700

    Update vogon-backend-startup.sh
    fixed formatting

--- docker_scripts/vogon-backend-startup.sh
+++ docker_scripts/vogon-backend-startup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 source /usr/src/app/host/env_secrets
-pip install -r requirements.txt
 service redis-server start
 service supervisor start
 cd /usr/src/app/host
+pip install -r requirements.txt
 python manage.py createcachetable
 python manage.py migrate
 
-if python manage.py test; then
+if python manage.py test
+then
+    echo "runs"
 else
-    return exit 1
+    exit 1
 fi
 tail -f /dev/null
 
