7d2b7e585295c1b90b4c16244a429041b0818ac3
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2021-04-30 11:34:46 -0700

    Add date appellation serializer

---
+++ annotations/views/date_appellation_views.py
@@ -0,0 +1,43 @@
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+
+from annotations.models import DateAppellation, DocumentPosition
+from annotations.serializers import DateAppellationSerializer, DocumentPositionSerializer
+
+class DateAppellationViewSet(viewsets.ModelViewSet):
+    queryset = DateAppellation.objects.all()
+    serializer_class = DateAppellationSerializer
+
+    def create(self, request):
+        data = request.data.copy()
+        position = data.pop('position', None)
+        if 'month' in data and data['month'] is None:
+            data.pop('month')
+        if 'day' in data and data['day'] is None:
+            data.pop('day')
+            
+        data['createdBy'] = request.user.id
+        
+        serializer = DateAppellationSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        instance = serializer.save()
+        
+        text_id = serializer.data.get('occursIn')
+        if position:
+            if type(position) is not DocumentPosition:
+                position_serializer = DocumentPositionSerializer(data=position)
+                position_serializer.is_valid(raise_exception=True)
+                position = position_serializer.save()
+
+            instance.position = position
+            instance.save()
+
+        instance.refresh_from_db()
+        reserializer = DateAppellationSerializer(instance, context={'request': request})
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            reserializer.data, 
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
