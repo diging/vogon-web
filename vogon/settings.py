fbe6564bb50c9503dbd3c06fd1640d4a3572d772
sudheerad9 <sudheerad9@gmail.com>
2022-03-15 11:24:57 -0600

    [VGNWB-248] clean code

--- vogon/settings.py
+++ vogon/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "secretsecret")
 DEBUG = os.environ.get('DEBUG', 'False')
 # DEBUG = True
 
-ALLOWED_HOSTS = ["amphora.asu.edu", "localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["amphora.asu.edu", "localhost"]
 
 # Application definition
 
